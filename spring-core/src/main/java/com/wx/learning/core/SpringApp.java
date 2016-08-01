57bc4f797eb4a36ce678aca657d57b1dcc177c2c
bangjin <bangjin.wang@gmail.com>
2016-08-01 22:36:47 +0800

    hsql改成内存模式

--- spring-core/src/main/java/com/wx/learning/core/SpringApp.java
+++ spring-core/src/main/java/com/wx/learning/core/SpringApp.java
@@ -1,15 +1,9 @@
 package com.wx.learning.core;
 
-import com.wx.learning.core.service.HSQLService;
-import com.wx.learning.core.service.impl.DefaultHSQLService;
 import com.wx.learning.core.service.impl.DefaultUserService;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.hsqldb.server.Server;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.PrintWriter;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class SpringApp {
@@ -17,6 +11,5 @@ public class SpringApp {
         ApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
         DefaultUserService userService = context.getBean("userService", DefaultUserService.class);
         System.out.println(userService.getUserById(123));
-        context.getBean("hsqlService", DefaultHSQLService.class).shutdown();
     }
 }
--- spring-core/src/main/java/com/wx/learning/core/service/HSQLService.java
+++
@@ -1,6 +0,0 @@
-package com.wx.learning.core.service;
-
-public interface HSQLService {
-    void start();
-    void shutdown();
-}
